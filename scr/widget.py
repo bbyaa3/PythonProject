4cc07bfda809cbbf3eb49edf7ff79cbc8b36d6d0
Фокин Андрей <andrefokin555@mail.ru>
2025-05-02 15:25:21 +0300

    коммит проверка работы

---
+++ scr/widget.py
@@ -0,0 +1,14 @@
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(account_info):
+    if account_info[:5].lower() == 'счет':
+        return account_info[:6] + get_mask_account(account_info[6:])
+    else:
+        return account_info[:-12] + get_mask_card_number(account_info[-12:])
+
+def get_date(date):
+    date_formate = f'{date[8:10]}.{date[5:7]}.{date[:4]}'
+    return date_formate
+
+print(mask_account_card('Maestro 1596837868705199'))
+print(get_date("2024-03-11T02:26:18.671407"))
